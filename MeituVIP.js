1a6a56d930e3e3b22c16b4488c03b4558b506b38
adminltqk28280828 <aquockhanhmedia@gmail.com>
2025-12-27 15:54:36 +0700

    Update MeituVIP.js

--- MeituVIP.js
+++ MeituVIP.js
@@ -1,44 +1,31 @@
 /*
- * @name: Meitu VIP & AdBlock Exclusive
+ * @name: Meitu VIP Fix Connection
  * @author: Quoc Khanh Media
- * @description: Mở khóa tính năng VIP Meitu và chặn quảng cáo startup
  */
 
 const url = $request.url;
 let body = $response.body;
 
-if (body) {
-    let obj = JSON.parse(body);
-
-    // 1. Xử lý Mở khóa VIP (Endpoint: vip/info, user/info)
-    if (url.includes("/vip/info") || url.includes("/user/info") || url.includes("/subs/info")) {
-        const vipData = {
-            "is_vip": true,
-            "vip_type": 1,
-            "expire_time": "2099-12-31 23:59:59",
-            "is_expire": false,
-            "subscription_status": 1,
-            "can_use_vip_feature": true,
-            "level": 9,
-            "status": 1,
-            "is_in_free_trial": false
-        };
-
-        if (obj.data) {
-            obj.data.vip_info = { ...obj.data.vip_info, ...vipData };
-            obj.data.is_vip = true;
-            obj.data.vip_type = 1;
+try {
+    if (body) {
+        let obj = JSON.parse(body);
+        if (url.includes("/vip/info") || url.includes("/user/info")) {
+            obj.data = {
+                ...obj.data,
+                "is_vip": true,
+                "vip_type": 1,
+                "expire_time": "2099-12-31 23:59:59",
+                "can_use_vip_feature": true
+            };
         }
+        // Chặn quảng cáo nhanh hơn
+        if (url.includes("/ad/get") || url.includes("/startup")) {
+            obj.data = [];
+        }
+        $done({ body: JSON.stringify(obj) });
+    } else {
+        $done({});
     }
-
-    // 2. Xử lý Chặn Quảng cáo (Endpoint: ad/get, startup)
-    if (url.includes("/ad/get") || url.includes("/startup") || url.includes("/launch")) {
-        if (obj.data) obj.data = [];
-        if (obj.ads) obj.ads = [];
-        if (obj.response) obj.response = {};
-    }
-
-    body = JSON.stringify(obj);
+} catch (e) {
+    $done({}); // Trả về dữ liệu gốc nếu có lỗi để tránh mất mạng
 }
-
-$done({ body });
